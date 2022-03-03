06454e95a682919eea6d3ab920f1033d89db662c
priyankaankireddypalli <85746649+priyankaankireddypalli@users.noreply.github.com>
2022-03-03 13:05:17 +0530

    Add files via upload

---
+++ database-demojpaintro/target/classes/data.sql
@@ -0,0 +1,5 @@
+
+
+insert into personjpa(id, name, location, birth_date) values (10001,'Priya','Hyderabad',sysdate());
+insert into personjpa(id, name, location, birth_date) values (10002,'James','New York',sysdate());
+insert into personjpa(id, name, location, birth_date) values (10003,'Pieter','Amssterdam',sysdate());
